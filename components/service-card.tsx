21fd8a1958d173ef386793be76da1793bf1bbe66
molmar <marton3molnar@gmail.com>
2025-06-19 02:54:42 +0200

    Fix: moving gradient background/frame

--- components/service-card.tsx
+++ components/service-card.tsx
@@ -1,4 +1,6 @@
 import { StaticImageData } from "next/image";
+import styles from './service-card.module.css'
+import { cn } from "@/lib/utils";
 
 interface Props{
     title: string;
@@ -7,24 +9,28 @@ interface Props{
 }
 
 export default function ServiceCard({title, description, image} : Props){
+
     return (
         <div
-            className="h-full w-full min-h-60 shadow-md/30 shadow-white rounded-2xl text-white cursor-default transition duration-300 hover:scale-110 hover:z-10"
+            className={
+                cn(
+                    "h-full w-full min-h-60 rounded-2xl text-white cursor-default transition duration-300 hover:scale-110 hover:z-2",
+                    styles._service_card
+                )
+            }
             style={{
-                backgroundImage: `url(${image.src})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                backgroundColor: 'rgba(0, 0, 0, 0.4)',
-                backgroundBlendMode: "overlay",
-            }}
+                '--card-background-image': `url(${image.src})`,
+            } as React.CSSProperties}
         >
-            <div className="h-full w-full flex flex-col justify-end p-5 rounded-2xl backdrop-blur-xs hover:backdrop-blur-none transition duration-300">
-                <h2 className="text-2xl font-bold drop-shadow-xl drop-shadow-black">
-                    {title}
-                </h2>
-                <p>
-                    {description}
-                </p>
+            <div className="">
+                <div className="h-full w-full flex flex-col justify-end p-5 rounded-2xl backdrop-blur-xs hover:backdrop-blur-none transition duration-300">
+                    <h2 className="text-2xl font-bold drop-shadow-xl drop-shadow-black">
+                        {title}
+                    </h2>
+                    <p>
+                        {description}
+                    </p>
+                </div>
             </div>
         </div>
     )
